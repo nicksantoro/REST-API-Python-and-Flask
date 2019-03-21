2efa180fd9404696cae0f9d7bc0bdc5ef70b8505
Nick Santoro <santoro.nick@gmail.com>
2019-03-20 23:41:40 -0700

    implementing endpoints

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 # special python variable __name__ gives each file a unique name
 app = Flask(__name__)
@@ -26,30 +26,57 @@ stores = [
 def home():
     return "Hello World!"
 
+
 # POST /store data: {name}
 @app.route('/store', methods=['POST'])
 def create_store():
-    pass
+    request_data = request.get_json()
+    new_store = {
+        'name': request_data['name'],
+        'items': []
+    }
+    stores.append(new_store)
+    return jsonify(new_store)
+
 
 # GET /store/<string:name>
 @app.route('/store/<string:name>')
 def get_store(name):
-    pass
+    for store in stores:
+        if store["name"] == name:
+            return jsonify(store)
+    return jsonify({"message": "error, store not found"})
+
 
 # GET /store
 @app.route('/store')
 def get_stores():
     return jsonify({'stores': stores})
 
+
 # POST /store/<string:name>/item
 @app.route('/store/<string:name>/item', methods=['POST'])
 def create_item_in_store(name):
-    pass
+    request_data = request.get_json()
+    for store in stores:
+        if store["name"] == name:
+            new_item = {
+                "name": request_data["name"],
+                "price": request_data["price"]
+            }
+            store["items"].append(new_item)
+            return jsonify(store)
+            # can also return jsonify(new_item)
+    return jsonify({"message": "error, store not found"})
+
 
 # GET /store/<string:name>/item
 @app.route('/store/<string:name>/item')
-def get_item_in_store(name):
-    return jsonify({item: stores['item']['name']})
+def get_items_in_store(name):
+    for store in stores:
+        if store["name"] == name:
+            return jsonify({"items": store["items"]})
+    return jsonify({"message": "store not found"})
 
 
 app.run(port=5000)
